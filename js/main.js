0ee02a1aa12de2a65efce60c5142320791decbd4
Pabel <pabel.pallab@gmail.com>
2020-10-28 23:06:57 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,69 @@
+
+// Fireup the plugins
+$(document).ready(function(){
+	
+	// initialise  slideshow
+	 $('.flexslider').flexslider({
+        animation: "slide",
+        start: function(slider){
+          $('body').removeClass('loading');
+        }
+      });
+
+});
+/**
+ * Handles toggling the navigation menu for small screens.
+ */
+( function() {
+	var button = document.getElementById( 'topnav' ).getElementsByTagName( 'div' )[0],
+	    menu   = document.getElementById( 'topnav' ).getElementsByTagName( 'ul' )[0];
+
+	if ( undefined === button )
+		return false;
+
+	// Hide button if menu is missing or empty.
+	if ( undefined === menu || ! menu.childNodes.length ) {
+		button.style.display = 'none';
+		return false;
+	}
+
+	button.onclick = function() {
+		if ( -1 == menu.className.indexOf( 'srt-menu' ) )
+			menu.className = 'srt-menu';
+
+		if ( -1 != button.className.indexOf( 'toggled-on' ) ) {
+			button.className = button.className.replace( ' toggled-on', '' );
+			menu.className = menu.className.replace( ' toggled-on', '' );
+		} else {
+			button.className += ' toggled-on';
+			menu.className += ' toggled-on';
+		}
+    };
+    $(".staff-list").owlCarousel({
+        items:4,
+        autoplay:false
+    });
+} )();
+(function ($) {
+	"üse strict";
+
+	$(document).ready(function(){
+		$('.owl-carousel').owlCarousel({
+			loop:true,
+			margin:10,
+			nav:true,
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:3
+				},
+				1000:{
+					items:5
+				}
+			}
+		})
+	  });
+
+}(jQuery));
